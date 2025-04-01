db6822358e4b2d8ab6790f4458d76c755ce7e32f
higortab <higor.tabraga@gmail.com>
2025-04-01 09:28:42 -0300

    FEAT: aula_01_04

---
+++ aula_01_04/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+  import { ref } from "vue";
+
+  const number = ref(0)
+  const numberTwo = ref(0)
+
+  function encrementar() {
+    number.value++
+  }
+
+  function decrementar() {
+    number.value--
+  }
+
+  function aumentar() {
+    numberTwo.value++
+  }
+
+  function diminuir() {
+    numberTwo.value--
+  }
+
+  
+
+</script>
+
+<template>
+<div>
+  <h1>Contador 1</h1>
+  <button @click="encrementar">+</button>
+  <button @click="decrementar">-</button>
+  <p>{{ number }}</p>
+</div>
+<div>
+  <h1>Contador 2</h1>
+  <button @click="aumentar">+</button>
+  <button @click="diminuir">-</button>
+  <p>{{ numberTwo }}</p>
+  <h2>Soma dos Contadores: {{ number + numberTwo }}</h2>
+</div>
+</template>
+
+<style scoped>
+
+</style>
