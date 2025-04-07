39957da16c4985213216f94b0e7bdcf662e5966e
higortab <higor.tabraga@gmail.com>
2025-04-07 11:57:19 -0300

    FEAT: término dos exercícios

--- aula_01_04/src/App.vue
+++ aula_01_04/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from "vue";
+  import { ref, computed } from "vue";
 
   const number = ref(0)
   const numberTwo = ref(0)
@@ -20,12 +20,15 @@
     numberTwo.value--
   }
 
-  
+  const soma = computed(() => number.value + numberTwo.value);
+  const maior10 = computed(() => soma.value >= 10);
+  const somaPar = computed(() => soma.value % 2 == 0);
 
 </script>
 
 <template>
-<div>
+<div class="html">
+  <div>
   <h1>Contador 1</h1>
   <button @click="encrementar">+</button>
   <button @click="decrementar">-</button>
@@ -36,10 +39,36 @@
   <button @click="aumentar">+</button>
   <button @click="diminuir">-</button>
   <p>{{ numberTwo }}</p>
-  <h2>Soma dos Contadores: {{ number + numberTwo }}</h2>
+</div>
+<div>
+  <h1>A soma dos dois contadores é: {{ soma }}</h1>
+</div>
+<div v-if="maior10">
+  <p>A soma é maior que 10</p>
+</div>
+<div v-else>
+  <p>A soma é menor que 10</p>
+</div>
+<div v-if="somaPar">
+  <p>A soma é par</p>
+</div>
+<div v-else>
+  <p>A soma é ímpar</p>
+</div>
+<div>
+  <button @click="mostrar = !mostrar">mudar</button>
+</div>
+<div v-if="mostrar">
+  <p>true</p>
+</div>
+<div v-else>
+  <p>false</p>
+</div>
 </div>
 </template>
 
 <style scoped>
-
+  .html {
+    align-items: center;
+  }
 </style>
