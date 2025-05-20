06843b3d202ad401b91dc5126c0c01c747cb63bc
higortab <higor.tabraga@gmail.com>
2025-05-20 09:21:12 -0300

    FEAT: Começo Exercicio 20/5

--- carrinho_prof/src/App.vue
+++ carrinho_prof/src/App.vue
@@ -5,6 +5,7 @@ import HeroComponent from '@/components/HeroComponent.vue'
 import FeaturedComponent from '@/components/FeaturedComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
 import CartComponent from './components/CartComponent.vue'
+import BooksListing from './components/BooksListing.vue'
 const showCart = ref(false)
 const cart = ref({
   items: [],
@@ -109,18 +110,7 @@ function addToCart(book) {
   <main v-else>
     <HeroComponent />
     <FeaturedComponent />
-    <section class="books">
-      <article class="book" v-for="book in books" :key="book.id">
-        <img :src="book.cover" :alt="book.title" />
-        <h2>{{ book.title }}</h2>
-        <p class="book-author">{{ book.author }}</p>
-        <span class="price-and-like">
-          <p class="book-price">R$ {{ book.price.toFixed(2) }}</p>
-          <span class="mdi mdi-heart-outline"></span>
-        </span>
-        <button @click="addToCart(book)"><span class="mdi mdi-cart"></span>Comprar</button>
-      </article>
-    </section>
+    <BooksListing :books="books" @add-to-cart="addToCart"/>
   </main>
   <FooterCompenent />
 </template>
@@ -128,44 +118,6 @@ function addToCart(book) {
 <style scoped>
 
 
-.books {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  padding: 5vh 8vw;
-
-  & .book {
-    display: flex;
-    flex-direction: column;
-    min-width: 300px;
-    width: calc(100% / 4 - 42px);
-    margin: 20px;
-
-    & h2 {
-      font-size: 1.5rem;
-      font-weight: 700;
-    }
-
-    & .book-author {
-      font-size: 1rem;
-    }
-
-    & .book-price {
-      font-size: 1.2rem;
-      font-weight: 700;
-    }
-
-    & .price-and-like {
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 20px;
-
-      & .mdi-heart-outline {
-        font-size: 1.3rem;
-        color: #27ae60;
-      }
-    }
-  }
-}
 
 
 
---
+++ exercicio_carrinho/src/components/BooksComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="books">
+      <article class="book" v-for="book in books" :key="book.id">
+        <img :src="book.cover" :alt="book.title" />
+        <h2>{{ book.title }}</h2>
+        <p class="book-author">{{ book.author }}</p>
+        <span class="price-and-like">
+          <p class="book-price">R$ {{ book.price.toFixed(2) }}</p>
+          <span class="mdi mdi-heart-outline"></span>
+        </span>
+        <button @click="addToCart(book)"><span class="mdi mdi-cart"></span>Comprar</button>
+      </article>
+    </section>
+</template>
+
+<style >
+
+</style>
